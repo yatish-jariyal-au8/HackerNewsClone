dfe04e43641b43f52c18155f180b98e50e004742
Yatish Jariyal <yatishjariyal@gmail.com>
2020-11-24 12:58:12 +0530

    added comments,stories and styling

---
+++ pages/stories.js
@@ -0,0 +1,25 @@
+import Story from '../components/Story.js';
+import view from '../utils/view.js';
+import baseUrl from '../utils/baseUrl.js';
+
+export default async function Stories(path) {
+  const stories = await getStories(path);
+  const hasStories = stories.length > 0;
+                    
+  view.innerHTML = `<div>
+    ${hasStories ? stories.map((story, i) => Story({ ...story, index: i + 1 })).join('') : 'No stories'}
+  </div>`;  
+}
+
+async function getStories(path) {
+  const isHomeRoute = path === '/';
+  const isNewRoute = path === '/new';
+  if (isHomeRoute) {
+    path = '/news';  
+  } else if (isNewRoute) {
+    path = '/newest';  
+  } 
+  const response = await fetch(`${baseUrl}${path}`);
+  const stories = await response.json();
+  return stories;
+}
