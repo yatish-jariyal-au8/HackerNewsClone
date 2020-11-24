8d36535bc8ae6e136d68ef8cc301fa5be0dc363a
Yatish Jariyal <yatishjariyal@gmail.com>
2020-11-24 14:56:39 +0530

    created favorites page, added functionality to add and remove favorites post

--- components/Story.js
+++ components/Story.js
@@ -1,25 +1,26 @@
 export default function Story(story) {
   return `
-<div class="story">
-    <div> 
-        <span class="gray">${story.index}</span>
-        <span class="upvote">▲</span>
-        <a href="${story.url}">${story.title}</a>
-        <span>(${story.domain})</span>
-    </div>
-    <div>
-    <div class="gray">
-      ${story.points} points by ${story.user} ${story.time_ago}
-      |
-      <a href="#/item?id=${story.id}">
-        ${story.comments_count} comments
-      </a>
-      |
-      <span class="favorite">
-        <img class="heart" src="https://icon.now.sh/heart/ccc">
-        Add To Favorites
-      </span>
-    </div>
-    </div>
-</div>`;
+      <div class="story">
+        <div> 
+          <span class="gray">${story.index || ""}</span>
+          <span class="upvote">▲</span>
+          <a href="${story.url}">${story.title}</a>
+          <span>(${story.domain})</span>
+        </div>
+        <div>
+          <div class="gray">
+            ${story.points} points by ${story.user} ${story.time_ago}
+            |
+            <a href="#/item?id=${story.id}">
+              ${story.comments_count} comments
+            </a>
+            |
+            <span class="favorite" data-story='${JSON.stringify(story)}'>
+              <img class="heart" src="heart-solid.svg">
+              ${story.isFavorite ? "Remove From Favorites" : "Add To Favorites"}
+            </span>
+          </div>
+        </div>
+      </div>
+    `;
 }
